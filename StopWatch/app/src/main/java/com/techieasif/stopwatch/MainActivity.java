83cf1cd68d2f973e576b8da8d51e81b42e47e819
Asif <techieasif@gmail.com>
2020-06-15 22:35:12 +0530

    Stop watch code added

--- StopWatch/app/src/main/java/com/techieasif/stopwatch/MainActivity.java
+++ StopWatch/app/src/main/java/com/techieasif/stopwatch/MainActivity.java
@@ -3,12 +3,53 @@ package com.techieasif.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        runTimer();
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onCLickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer(){
+        final TextView textView = (TextView) findViewById(R.id.tv);
+
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format("%d:%02d:%02d", hours,minutes,secs);
+                textView.setText(time);
+                if(running){
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+
+
     }
 }
